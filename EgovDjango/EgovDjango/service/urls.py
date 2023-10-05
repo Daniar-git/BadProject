541ee11a3ba05143cf1b670fe53b78ecaa698944
Daniar-git <daniar.78@inbox.ru>
2023-10-05 12:16:33 +0600

    Egov Login

---
+++ EgovDjango/EgovDjango/service/urls.py
@@ -0,0 +1,6 @@
+# from . import views
+# from django.urls import path
+#
+# urlpatterns = [
+#     path('forma2/', views.UserLogin.as_view()),
+# ]
--- EgovDjango/EgovDjango/user/views.py
+++ EgovDjango/EgovDjango/user/views.py
@@ -1,5 +1,7 @@
 import json
+import time
 
+import requests
 from rest_framework import views, status
 from rest_framework.response import Response
 from selenium import webdriver
@@ -11,25 +13,61 @@ class UserLogin(views.APIView):
     def post(self, request):
         iin = request.data.get('iin')
         password = request.data.get('password')
-        browser = webdriver.Firefox()
-        browser.get("https://idp.egov.kz/idp/sign-in")
-        browser.find_element('xpath', '//*[@id="username"]').send_keys(iin)
-        browser.find_element('xpath', '//*[@id="login-password-nav"]/form/div[3]/div/div/table/tbody/tr[2]/td/label/input').send_keys(password)
-        browser.find_element('xpath', '//*[@id="login-password-nav"]/form/div[3]/div/div/table/tbody/tr[3]/td/input').click()
-        print(browser.session_id)
-        print(browser.get_cookies())
-        return Response(json.dumps(browser.get_cookies()), status=status.HTTP_200_OK)
+        s = requests.Session()
+        s.get("https://idp.egov.kz/idp/sign-in")
+        s.headers['Host'] = 'idp.egov.kz'
+        s.headers['Origin'] = 'https://idp.egov.kz/'
+        s.headers['Referer'] = 'https://idp.egov.kz/idp/sign-in'
+        payload = {
+            'lvl': '2',
+            'username': iin,
+            'password': password,
+            'submit': 'Войти в систему'
+        }
+        s.post("https://idp.egov.kz/idp/validate-pass", data=payload)
+        return Response(s.cookies, status=status.HTTP_200_OK)
 
 
 class LoginCode(views.APIView):
     def post(self, request):
-        browser = webdriver.Firefox()
-        browser.get("https://idp.egov.kz/idp/sign-in")
         code = request.data.get('code')
-        cookie = request.data.get('cookie')
-        session = request.data.get('session')
-        browser.add_cookie(cookie)
-        browser.session_id = session
-        browser.find_element('xpath', '//*[@id="confirmationOTPCode"]').send_keys(code)
-        browser.find_element('xpath', '//*[@id="submitStep2"]').click()
-        return Response({"succes": "True"}, status=status.HTTP_200_OK)
+        cookies = request.data.get('cookies')
+        iin = request.data.get('iin')
+        phone = request.data.get('phone')
+        s = requests.Session()
+        for cookie_name, cookie_value in cookies.items():
+            s.cookies.set(cookie_name, cookie_value)
+
+        payload2 = {
+            'url': '',
+            'username': iin,
+            'phone': phone,
+            'lvl': 2,
+            'code': code,
+            'submit': 'Войти в систему'
+        }
+        s.post("https://idp.egov.kz/idp/otp-login.do", data=payload2, allow_redirects=False)
+
+        s.get('https://egov.kz/cms/ru')
+        s.headers['Host'] = 'my.egov.kz'
+        s.get('https://my.egov.kz/#/')
+        browser = webdriver.Firefox()
+        try:
+            browser.get("https://my.egov.kz/#/")
+
+            for cookie in s.cookies:
+                cookie_dict = {
+                    "name": cookie.name,
+                    "value": cookie.value,
+                }
+                print(cookie_dict)
+                browser.add_cookie(cookie_dict)
+            time.sleep(5)
+            browser.refresh()
+            print(browser.get_cookies())
+
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+        input("Press Enter to exit...")
+        return Response(s.cookies, status=status.HTTP_200_OK)
