e3aea18ebac0fc4146aa4549b8f7060025c38d7d
Daniar-git <daniar.78@inbox.ru>
2023-10-07 17:00:53 +0600

    eggoving

--- EgovDjango/EgovDjango/service/urls.py
+++ EgovDjango/EgovDjango/service/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('forma2/', views.FormaView.as_view()),
+    # path('forma2/credentials/', views.FormaCredentialsView.as_view()),
+    path('forma2/code/', views.FormaCodeView.as_view()),
 ]
